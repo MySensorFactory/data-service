9f0614ff6e355d7a3962223013dd9c54149877b3
Damian Wójcik <damian.a.wojcik@comarch.pl>
2023-09-21 16:58:45 +0200

    Added string wrappers

--- src/main/java/com/factory/domain/SensorLabel.java
+++ src/main/java/com/factory/domain/SensorLabel.java
@@ -3,10 +3,12 @@ package com.factory.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor(staticName = "of")
 @EqualsAndHashCode
+@NoArgsConstructor
 public class SensorLabel {
     private String label;
 }
--- src/main/java/com/factory/domain/SensorType.java
+++ src/main/java/com/factory/domain/SensorType.java
@@ -3,9 +3,11 @@ package com.factory.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
 @EqualsAndHashCode
 public class SensorType {
     private String type;
